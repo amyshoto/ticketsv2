4572e54ed5f106ee596b5a28369cbacaae44e22e
amyshoto <144857528+amyshoto@users.noreply.github.com>
2024-05-27 14:03:25 -0600

    Update conexionbd.php

--- php/conexionbd.php
+++ php/conexionbd.php
@@ -1,5 +1,5 @@
 <?php 
-	$conexion = pg_connect("host=10.25.96.150 dbname=tickets user=postgres password=S0p0rt31234");
+	$conexion = pg_connect("host=10.25.96.152 dbname=tickets user=postgres password=S0p0rt31234");
 	if($conexion){
 	} else {
 		echo "No funciona la conexion";
